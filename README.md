f5012d94533178428ac30c28fd034f5ebf12cc77
david <david@itarverne.fr>
2026-05-11 12:04:27 +0200

    add Depends() on FastAPI controler

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ You can found several live talking about :
 - Service pattern on [YouTube channel](https://www.youtube.com/live/GGYWhoxGCo8) with this [commit](https://github.com/general03/live-coding-3-tiers/commit/1f3edf00add17717468be3a4bc578dd3c647ffe7)
 - FastAPI lifespan on [YouTube channel](https://www.youtube.com/live/jeBF7llkPjI) with this [commit](https://github.com/general03/live-coding-3-tiers/commit/8d7b9bed46c536a37163a7f14d6a9287bf51066d)
 - FastAPI error handler exception on [YouTube channel](https://www.youtube.com/live/QTl5Mbt2zP0) with this [commit](https://github.com/general03/live-coding-3-tiers/commit/4b0437914c841321519f51ebe818ddfe4e780ec8)
+- FastAPI `Depends()` on [YouTube channel](https://www.youtube.com/live/R7dXzSJXnjo) with this [commit](https://github.com/general03/live-coding-3-tiers/commit/4bac2adfde1ab5603f296994c7faba60d5dd8283)
 
 The initial ugly `main.py` was moved on `main_legacy.py`
 
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from contextlib import asynccontextmanager
 import sqlite3
 from exception import AccessDataError, AppError
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
+
+from ports.abstract_product_repository import AbstractProductRepository
 from services.product_service import NotFoundProduct, ProductService
 from fastapi.responses import JSONResponse
 
@@ -11,7 +13,7 @@ from repositories.sqlite_product_repository import SqliteProductRepository
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     DATABASE = "ecommerce.db"
-    app.state.db = sqlite3.connect(DATABASE)
+    app.state.db = sqlite3.connect(DATABASE, check_same_thread=False)
 
     yield
 
@@ -32,12 +34,23 @@ async def sql_error_handler(request: Request, e: AppError):
 async def app_error_handler(request: Request, e: AppError):
     return JSONResponse(status_code=400, content={"data": str(e)})
 
+def get_database_connection() -> sqlite3.Connection:
+    return app.state.db
+
+def get_product_repository(
+    db: sqlite3.Connection = Depends(get_database_connection)
+) -> AbstractProductRepository:
+    return SqliteProductRepository(db)
+
+def get_product_service(
+    repo: AbstractProductRepository = Depends(get_product_repository)
+) -> ProductService:
+    return ProductService(repo=repo)
+
 
 @app.get("/products/{id}")
-async def product_infos(id: int):
-    repo = SqliteProductRepository(app.state.db)
-
-    product_service = ProductService(repo=repo)
+async def product_infos(id: int, product_service: ProductService = Depends(get_product_service)
+):
     stock_product = product_service.get_by_id(id)
 
     return JSONResponse(content={"data": {"stock": stock_product}})
